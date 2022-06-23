6507c9570469095341f988f6e8c569a0c76963e3
sachin <sachin.dabgar@dataweave.com>
2022-06-23 15:01:31 +0530

    app.run changed

--- app.py
+++ app.py
@@ -47,5 +47,6 @@ def update_date(uid,fi,val):
     
 if __name__ == '__main__':
   
-    app.run(debug = False, host='0.0.0.0')
+    # app.run(debug = False, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     
