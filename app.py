0078f8c580aedebe9746f95c4e9cbb87761faf21
sachin <sachin.dabgar@dataweave.com>
2022-06-24 13:17:43 +0530

    have changes

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ if __name__ == '__main__':
     # app.run(debug = False, host='0.0.0.0')
     # app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
     # app.run()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0')
     
