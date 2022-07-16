3fb24ff402acae17cd9463a5ae02b01282864b64
vish <vishpande2900@gmail.com>
2022-07-16 23:39:35 +0530

    ui connect part1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import json,os
 import requests
 import pyrebase
@@ -17,27 +17,29 @@ config={
   "appId": "1:1021317922890:web:71550b203e9eb04f8de6b3",
   "measurementId": "G-RGCMC81BWC"}
 
+firebase=pyrebase.initialize_app(config)
+db = firebase.database()
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-        return "<h1>Hello there </h1>"
+        return render_template('index.html')
 
 @app.route('/post_json', methods=['POST'])
 def process_json():
     content_type = request.headers.get('Content-Type')
     if (content_type == 'application/json'):
-        json = request.json
-        return json
+        json_data = request.json
+        return db.update(json_data)
     else:
         return 'Content-Type not supported!'
 
 
-@app.route('/update/<string:fid>:<string:val>', methods=['GET'])
-def update_field(fid,val):  
-    firebase=pyrebase.initialize_app(config)
-    data = {fid:val}
-    db = firebase.database()
-    return  db.update(data) 
+# @app.route('/update/<string:fid>:<string:val>', methods=['GET'])
+# def update_field(fid,val):  
+#     firebase=pyrebase.initialize_app(config)
+#     data = {fid:val}
+#     db = firebase.database()
+#     return  db.update(data) 
 
     
 if __name__ == '__main__':
