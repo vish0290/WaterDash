04753fefcedcdfbae2c1e0f1bf2e35f360a396f7
vish <vishpande2900@gmail.com>
2022-07-17 16:13:58 +0530

    Co-authored-by: Sachin Dabgar <Bosscoder1325@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, redirect
 import json,os
 import requests
 import pyrebase
@@ -6,6 +6,7 @@ from test2 import update_field
 # creating a Flask app
 app = Flask(__name__)
 session = False
+cid = ""
 
 config={  
 "apiKey": "AIzaSyCc0veJ4Kez3iLS8U-qjxhOMFwnzA7WZ7U",
@@ -24,7 +25,61 @@ raw = json.loads(json.dumps(db.child('data').get().val()))
 
 @app.route('/', methods = ['GET', 'POST'])
 def home():
-        return render_template('index.html')
+    if session:
+        global cid
+        current_month = "04-2022"
+        usne_enter_kiya = cid
+        # Name
+        # Address
+        # Bill amount
+        # Status
+        
+        data_dic = {
+            'name': raw[usne_enter_kiya]["name"],
+            "address" : raw[usne_enter_kiya]["address"],
+            "bill_amount" : raw[usne_enter_kiya]['bill'][current_month]["bill_amount"],
+            "status" : raw[usne_enter_kiya]['bill'][current_month]["status"]
+        }
+
+        print(data_dic)
+        return render_template('index.html', data = data_dic)
+    return redirect('/login')
+
+@app.route('/logs',methods=['GET','POST'])
+def logs():
+    if session:
+        current_month = "04-2022"
+        usne_enter_kiya="user1"
+        # Name
+        # Address
+        # Bill amount
+        # Status
+        # Date Name Address Bill Status 
+        
+        data_dic = {
+            'name': raw[usne_enter_kiya]["name"],
+            "address" : raw[usne_enter_kiya]["address"],
+            "bill_amount" : raw[usne_enter_kiya]['bill'][current_month]["bill_amount"],
+            "status" : raw[usne_enter_kiya]['bill'][current_month]["status"]
+        }
+        
+        headings = ['Date','Name','Address','Bill Amount','Status']
+       
+        data_got = []
+
+        dates = raw[usne_enter_kiya]['bill'].keys()
+
+        for i in dates:
+            temp = []
+            temp.append(i)
+            temp.append(raw[usne_enter_kiya]["name"])
+            temp.append(raw[usne_enter_kiya]["address"])
+            temp.append(raw[usne_enter_kiya]['bill'][i]["bill_amount"])
+            temp.append(raw[usne_enter_kiya]['bill'][i]["status"])
+            data_got.append(tuple(temp))
+
+        return render_template('logs.html', data = data_dic, heading=headings, rows=data_got)
+    
 
 @app.route('/post_json', methods=['POST'])
 def process_json():
@@ -42,8 +97,16 @@ def login():
         passw = request.form.get('passw')
         if uid in list(raw.keys()):
             if passw == raw[uid]['passw']:
+                global session, cid
                 session = True
-                return render_template('index.html')
+                cid =  uid
+                return redirect('/')
+            else:
+                print('Wrong password')
+        else:
+            print('No user')
+    else:
+        print('post not working')
     return render_template('login.html')
         
 # @app.route('/update/<string:fid>:<string:val>', methods=['GET'])
--- test.py
+++ test.py
@@ -1,9 +1,52 @@
-import json
+from flask import Flask, jsonify, request, render_template, redirect
+import json,os
+import requests
+import pyrebase
+from test2 import update_field
+# creating a Flask app
+app = Flask(__name__)
+session = False
 
-f = open('demo.json')
-raw = json.load(f)
-maindata = raw['data']
-k = list(maindata.keys())
-print(maindata['user1']['passw'])
-if 'user1' in k and 1234 == maindata['user1']['passw']:    
-  print('true')
+config={  
+"apiKey": "AIzaSyCc0veJ4Kez3iLS8U-qjxhOMFwnzA7WZ7U",
+  "authDomain": "fir-46336.firebaseapp.com",
+  "databaseURL": "https://fir-46336-default-rtdb.asia-southeast1.firebasedatabase.app",
+  "projectId": "fir-46336",
+  "storageBucket": "fir-46336.appspot.com",
+  "messagingSenderId": "1021317922890",
+  "appId": "1:1021317922890:web:71550b203e9eb04f8de6b3",
+  "measurementId": "G-RGCMC81BWC"}
+
+firebase=pyrebase.initialize_app(config)
+db = firebase.database()
+raw = json.loads(json.dumps(db.child('data').get().val()))
+
+current_month = "04-2022"
+usne_enter_kiya="user2"
+# Name
+# Address
+# Bill amount
+# Status
+# Date Name Address Bill Status 
+
+data_dic = {
+    'name': raw[usne_enter_kiya]["name"],
+    "address" : raw[usne_enter_kiya]["address"],
+    "bill_amount" : raw[usne_enter_kiya]['bill'][current_month]["bill_amount"],
+    "status" : raw[usne_enter_kiya]['bill'][current_month]["status"]
+}
+
+headings = ['Date','Name','Address','Bill Amount','Status']
+
+data_got = []
+
+dates = raw[usne_enter_kiya]['bill'].keys()
+
+for i in dates:
+      temp = []
+      temp.append(i)
+      temp.append(raw[usne_enter_kiya]["name"])
+      temp.append(raw[usne_enter_kiya]["address"])
+      temp.append(raw[usne_enter_kiya]['bill'][i]["bill_amount"])
+      temp.append(raw[usne_enter_kiya]['bill'][i]["status"])
+      data_got.append(tuple(temp))
