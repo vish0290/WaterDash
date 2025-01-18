91ece95bb327f8a02273480f1f49b0c92cbaa0e8
Vishwanath A Sardeshpande <62197684+vish0290@users.noreply.github.com>
2025-01-19 00:02:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,15 +11,7 @@ app = Flask(__name__)
 session = False
 cid = ""
 
-config={  
-"apiKey": "AIzaSyCc0veJ4Kez3iLS8U-qjxhOMFwnzA7WZ7U",
-  "authDomain": "fir-46336.firebaseapp.com",
-  "databaseURL": "https://fir-46336-default-rtdb.asia-southeast1.firebasedatabase.app",
-  "projectId": "fir-46336",
-  "storageBucket": "fir-46336.appspot.com",
-  "messagingSenderId": "1021317922890",
-  "appId": "1:1021317922890:web:71550b203e9eb04f8de6b3",
-  "measurementId": "G-RGCMC81BWC"}
+
 
 firebase=pyrebase.initialize_app(config)
 db = firebase.database()
