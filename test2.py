977f3df94fb04f54deeca1a77c8c16fd41019fa0
Vishwanath A Sardeshpande <62197684+vish0290@users.noreply.github.com>
2025-01-19 00:03:46 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,13 +1,4 @@
 import pyrebase
-config={  
-"apiKey": "AIzaSyCc0veJ4Kez3iLS8U-qjxhOMFwnzA7WZ7U",
-  "authDomain": "fir-46336.firebaseapp.com",
-  "databaseURL": "https://fir-46336-default-rtdb.asia-southeast1.firebasedatabase.app",
-  "projectId": "fir-46336",
-  "storageBucket": "fir-46336.appspot.com",
-  "messagingSenderId": "1021317922890",
-  "appId": "1:1021317922890:web:71550b203e9eb04f8de6b3",
-  "measurementId": "G-RGCMC81BWC"}
 
 
 def update_field(field,value):  
